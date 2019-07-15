3b7aea4cc106a9ffe6538ca4e86af29c47efe527
aaroneASC5 <aaronessien@icloud.com>
2019-07-15 16:04:32 -0400

    shapes

---
+++ mario.js
@@ -0,0 +1,15 @@
+function setup(){
+    createCanvas(700,700);
+    fill(100, 100, 100);
+}
+
+function mouseDragged(){
+    circle(mouseX, mouseY, 100, 120);
+
+    rect 
+}
+
+function draw(){
+    rect(400, 400, 100, 100 )
+    ellipse(125, 125, 100, 100)
+}
